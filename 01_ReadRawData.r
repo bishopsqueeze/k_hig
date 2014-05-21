7b228f2c9f17ce4bbe9dee217dbb8d58b6fd6c85
bishopsqueeze <alexstephens@db.local>
2014-05-20 20:52:12 -0400

    Project start

---
+++ 01_ReadRawData.r
@@ -0,0 +1,49 @@
+##------------------------------------------------------------------
+## From the kaggle website:
+##------------------------------------------------------------------
+
+##------------------------------------------------------------------
+## Load libraries
+##------------------------------------------------------------------
+library(data.table)
+
+##------------------------------------------------------------------
+## Clear the workspace
+##------------------------------------------------------------------
+rm(list=ls())
+
+##------------------------------------------------------------------
+## Set the working directory
+##------------------------------------------------------------------
+setwd("/Users/alexstephens/Development/kaggle/higgs/data/raw")
+
+##------------------------------------------------------------------
+## Read the test and training datasets
+##------------------------------------------------------------------
+
+## read the raw data
+test.raw    <- read.csv("test.csv", header=TRUE)
+train.raw   <- read.csv("training.csv", header=TRUE)
+
+## lowercase the headers
+colnames(test.raw)  <- tolower(colnames(test.raw))
+colnames(train.raw) <- tolower(colnames(train.raw))
+
+##------------------------------------------------------------------
+## Save results as a data.table
+##------------------------------------------------------------------
+
+## create data tables
+test.dt     <- data.table(test.raw)
+train.dt    <- data.table(train.raw)
+
+## define keys
+setkey(test.dt, eventid)
+setkey(train.dt, eventid)
+
+## change to a new data directory
+setwd("/Users/alexstephens/Development/kaggle/higgs/data/proc")
+
+## save results to separate files given the size
+save(test.dt,  file="01_HiggsRawTest.Rdata")
+save(train.dt, file="01_HiggsRawTrain.Rdata")
