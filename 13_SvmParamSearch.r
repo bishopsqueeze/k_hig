9794a8d8d4a6ce6b5a86cf6bd04cfc8004fce8d8
bishopsqueeze <bishop.squeeze@gmail.com>
2014-05-24 09:51:53 -0400

    Updates

---
+++ 13_SvmParamSearch.r
@@ -0,0 +1,104 @@
+##------------------------------------------------------------------
+## From the kaggle website:
+##------------------------------------------------------------------
+
+##------------------------------------------------------------------
+## Load libraries
+##------------------------------------------------------------------
+library(data.table)
+library(caret)
+library(foreach)
+library(doMC)
+
+##------------------------------------------------------------------
+## register cores
+##------------------------------------------------------------------
+registerDoMC(4)
+
+##------------------------------------------------------------------
+## Clear the workspace
+##------------------------------------------------------------------
+rm(list=ls())
+
+##------------------------------------------------------------------
+## Set the working directory
+##------------------------------------------------------------------
+setwd("/Users/alexstephens/Development/kaggle/higgs/data/proc")
+
+##------------------------------------------------------------------
+## Source the utilities file
+##------------------------------------------------------------------
+source("/Users/alexstephens/Development/kaggle/higgs/k_hig/00_Utilities.r")
+
+##------------------------------------------------------------------
+## Load the training data
+##------------------------------------------------------------------
+loadfile <- "04_HiggsTrainExRbcLc.Rdata"
+load(loadfile)
+
+if (loadfile == c("04_HiggsTrainExRbcLc.Rdata")) {
+    trainDescr  <- train.ex.rbc.lc
+    trainClass  <- train.eval
+} else if (loadfile == c("04_HiggsTrainExRbcNas.Rdata")) {
+    trainDescr  <- train.ex.rbc.nas
+    trainClass  <- train.eval
+}
+
+##******************************************************************
+## Main
+##******************************************************************
+
+##------------------------------------------------------------------
+## Transform data to data.frame for the fitting procedure
+##------------------------------------------------------------------
+trainClass.df   <- as.data.frame(trainClass)
+trainDescr.df   <- as.data.frame(trainDescr)
+
+##------------------------------------------------------------------
+## Use a subset of the available data for the parameter search phase
+##------------------------------------------------------------------
+
+## number of rows in the training data
+nr  <- dim(trainDescr.df)[1]
+
+## number of samples to use for search
+ns  <- 10000
+
+## define the partition index
+set.seed(88888888)
+smp.idx    <- createDataPartition(
+                    y=trainClass.df[,c("label")],
+                    times=1,
+                    p = (ns/nr),
+                    list = TRUE)
+
+##------------------------------------------------------------------
+## set-up the fit parameters using the pre-selected (stratified) samples
+##------------------------------------------------------------------
+num.cv      <- 3
+num.repeat  <- 1
+num.total   <- num.cv * num.repeat
+
+## define the fit parameters
+fitControl <- trainControl(
+                    method="repeatedcv",
+                    number=num.cv,
+                    repeats=num.repeat,
+                    verboseIter=TRUE,
+                    classProbs=TRUE)
+
+
+##------------------------------------------------------------------
+## perform the fit
+##  - "rf" required a purging of the missings
+##------------------------------------------------------------------
+tmp.fit <- try(train(   x=trainDescr.df[smp.idx[[1]],-1],
+                        y=trainClass.df[smp.idx[[1]],c("label")],
+                        method="svmRadial",
+                        trControl=fitControl,
+                        verbose=TRUE,
+                        tuneLength=3))
+
+
+
+
