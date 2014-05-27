c1a06ba8a4d29ac70b35c588259303d4c82b3eca
bishopsqueeze <alexstephens@db.local>
2014-05-27 19:52:52 -0400

    Filename changes

---
+++ 99_sketch copy.r
@@ -0,0 +1,133 @@
+##------------------------------------------------------------------
+## From the kaggle website:
+##------------------------------------------------------------------
+
+##------------------------------------------------------------------
+## Load libraries
+##------------------------------------------------------------------
+library(data.table)
+library(caret)
+library(foreach)
+library(doMC)
+
+##------------------------------------------------------------------
+## register cores
+##------------------------------------------------------------------
+registerDoMC(4)
+
+##------------------------------------------------------------------
+## Clear the workspace
+##------------------------------------------------------------------
+rm(list=ls())
+
+##------------------------------------------------------------------
+## Set the working directory
+##------------------------------------------------------------------
+setwd("/Users/alexstephens/Development/kaggle/higgs/data/proc")
+
+##------------------------------------------------------------------
+## Source the utilities file
+##------------------------------------------------------------------
+source("/Users/alexstephens/Development/kaggle/higgs/k_hig/00_Utilities.r")
+
+##------------------------------------------------------------------
+## Load the training data
+##------------------------------------------------------------------
+loadfile <- "04_HiggsTrainExRbcLcNumNa.Rdata"
+load(loadfile)
+
+if (loadfile == c("04_HiggsTrainExRbcLc.Rdata")) {
+    trainDescr  <- train.ex.rbc.lc
+    trainClass  <- train.eval
+} else if (loadfile == c("04_HiggsTrainExRbcNas.Rdata")) {
+    trainDescr  <- train.ex.rbc.nas
+    trainClass  <- train.eval
+} else if (loadfile == c("04_HiggsTrainExRbcLcNumNa.Rdata")) {
+    trainDescr  <- train.ex.rbc.lc.numna
+    trainClass  <- train.eval
+}
+
+
+##******************************************************************
+## Main
+##******************************************************************
+
+##------------------------------------------------------------------
+## Transform data to data.frame for the fitting procedure
+##------------------------------------------------------------------
+trainClass.df   <- as.data.frame(trainClass)
+trainDescr.df   <- as.data.frame(trainDescr)
+
+##------------------------------------------------------------------
+## Use a subset of the available data for the parameter search phase
+##------------------------------------------------------------------
+
+## define the fraction to use as a hold-out sample
+p_ho    <- 0.20
+
+## define a partition index
+set.seed(88888888)
+samp.idx    <- createDataPartition(
+                            y=trainClass.df[,c("label")],
+                            times=1,
+                            p = (1-p_ho),
+                            list = TRUE)
+
+## split data into training & hold-out
+holdDescr    <- trainDescr.df[ -samp.idx$Resample1, ]
+holdClass    <- trainClass.df[ -samp.idx$Resample1, ]
+sampDescr    <- trainDescr.df[  samp.idx$Resample1, ]
+sampClass    <- trainClass.df[  samp.idx$Resample1, ]
+
+
+##------------------------------------------------------------------
+## set-up the fit parameters using the pre-selected (stratified) samples
+##------------------------------------------------------------------
+num.cv      <- 10
+num.repeat  <- 1
+num.total   <- num.cv * num.repeat
+
+## define the fit parameters
+fitControl <- trainControl(
+                    method="cv",
+                    number=num.cv,
+                    verboseIter=TRUE,
+                    savePredictions=FALSE)
+
+##------------------------------------------------------------------
+## perform the fit
+##------------------------------------------------------------------
+tmp.fit <- try(train(   x=sampDescr[,-1],
+                        y=sampClass[,c("label")],
+                        method="gbm",
+                        trControl=fitControl,
+                        verbose=TRUE,
+                        tuneGrid=data.frame(.interaction.depth=6, .n.trees=500, .shrinkage=0.01)))
+
+
+
+
+score.os    <- predict(tmp.fit, newdata=holdDescr[,-1], type="prob")[,c("s")]
+class.os    <- predict(tmp.fit, newdata=holdDescr[,-1])
+
+cuts.os     <- quantile(score.os, probs=seq(0,1,0.01))
+cuts.ams    <- sapply(cuts.os, calcAmsCutoff, score.os, holdClass$label, holdClass$weight)
+
+calcAms(class.os, holdClass$label, holdClass$weight)
+
+
+save(tmp.fit, sampDescr, sampClass, holdDescr, holdClass, file="gbm_d6_t500_s01.Rdata")
+
+##ggplot(train.dt, aes(x=myscore, fill=label)) + geom_histogram(binwidth=1, alpha=.5, position="identity")
+
+
+
+knnFit <- train(Species ~ ., data = iris, method = "knn",
+trControl = trainControl(method = "cv"))
+
+rdaFit <- train(Species ~ ., data = iris, method = "rda",
+trControl = trainControl(method = "cv"))
+
+
+
+
