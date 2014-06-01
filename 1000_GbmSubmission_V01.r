16002572ebffc5cad3f9c3a7bcc87606e4409423
bishopsqueeze <alexstephens@db.local>
2014-06-01 17:26:14 -0400

    Fit checks

--- 1000_GbmSubmission_V01.r
+++ 1000_GbmSubmission_V01.r
@@ -9,6 +9,8 @@ library(data.table)
 library(caret)
 library(foreach)
 library(doMC)
+library(gbm)
+library(plyr)
 
 ##------------------------------------------------------------------
 ## register cores
@@ -38,14 +40,13 @@ testFile    <- "04_HiggsTestExRbcLcNumNa.Rdata"
 
 load(trainFile)
 if (trainFile == c("04_HiggsTrainExRbcLcNumNa.Rdata")) {
-    trainDescr  <- train.ex.rbc.lc.numna
-    trainClass  <- train.eval
+    trainDescr  <- as.data.frame(train.ex.rbc.lc.numna)
+    trainClass  <- as.data.frame(train.eval)
 }
 
 load(testFile)
 if (testFile == c("04_HiggsTestExRbcLcNumNa.Rdata")) {
-    testDescr  <- test.ex.rbc.lc.numna
-    #testClass  <- test.eval
+    testDescr  <- as.data.frame(test.ex.rbc.lc.numna)
 }
 
 
@@ -56,12 +57,49 @@ if (testFile == c("04_HiggsTestExRbcLcNumNa.Rdata")) {
 
 ## Version 1
 v01.dir <- "/Users/alexstephens/Development/kaggle/higgs/data/proc/gbm_full_depth9_trees450_shrink0.05_V01"
+
+## Version 3
 v03.dir <- "/Users/alexstephens/Development/kaggle/higgs/data/proc/gbm_full_depth9_trees450_shrink0.05_V03"
+
+## Version 4
 v04.dir <- "/Users/alexstephens/Development/kaggle/higgs/data/proc/gbm_full_depth9_trees450_shrink0.05_V04"
 
 
+##------------------------------------------------------------------
+## Review Version_01 fits
+##------------------------------------------------------------------
+## Note that CV was performed on varying subsets, so I can't
+## compare results across models
+##------------------------------------------------------------------
+v01.files   <- dir(v01.dir)[grep(".Rdata$", dir(v01.dir))]
+v01.num     <- length(v01.files)
+v01.seq     <- seq(0,1,0.01)
+v01.ams     <- matrix(0,nrow=length(v01.seq),ncol=v01.num)
 
-(loadfile == c("04_HiggsTrainExRbcLcNumNa.Rdata")) {
-    trainDescr  <- train.ex.rbc.lc.numna
-    trainClass  <- train.eval
+for (i in 1:v01.num) {
+    
+    tmp.file    <- paste0(v01.dir,"/",v01.files[i])
+    load(tmp.file)
+    
+    ## in this case, the sample index represents the training data
+    holdDescr   <- trainDescr[-samp.idx$Resample1,]
+    holdClass   <- trainClass[-samp.idx$Resample1,]
+    
+    ## score the hold-out sample & compute the AMS curve
+    tmp.score    <- predict(tmp.fit, newdata=holdDescr[,-1], type="prob")[,c("s")]
+    tmp.pred     <- predict(tmp.fit, newdata=holdDescr[,-1])
+    tmp.breaks   <- quantile(tmp.score, probs=v01.seq)
+    tmp.ams      <- sapply(tmp.breaks, calcAmsCutoff, tmp.score, holdClass$label, holdClass$weight)
+    v01.ams[,i]  <- tmp.ams
+    
+    if (i == 1) {
+        plot(v01.seq, v01.ams[,i], type="b", col=i)
+    } else {
+        points(v01.seq, v01.ams[,i], type="b", col=i)
+    }
 }
+
+
+
+
+
